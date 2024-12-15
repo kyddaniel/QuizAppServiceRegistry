3caebc3ad3b7005341638e80499a389ba53237df
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-15 11:45:07 +0800

    Initial commit

---
+++ src/main/java/com/kyddaniel/QuizAppServiceRegistry/QuizAppServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.QuizAppServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppServiceRegistryApplication.class, args);
+	}
+
+}
